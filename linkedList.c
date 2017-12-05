12fb8a520b5a34abbf0ea8e0bb870aa663e8ccd0
knockinHeavenDoor <61239289@qq.com>
2017-12-06 00:33:33 +0800

    linkedList 停止工作，明天解决

--- linkedList.c
+++ linkedList.c
@@ -2,22 +2,30 @@
 // Created by xuhui on 2017/12/4.
 //
 #include "linkedList.h"
-
+int size;
 int main(){
     LinkedList list;
     create(list,5);
+    while((list->next) != NULL){
+        printf("data:%d",(*list).data);
+        list = list->next;
+    }
+    insert(list,3,9);
     int h = 3;
-    get(list,2,&h);
+    get(list,3,&h);
     printf("%d",h);
+    return 0;
 }
 
 void create(LinkedList l,int n){
     l = (LinkedList)malloc(sizeof(LNode));
     l -> next = NULL;
+    l -> data = 0;
     for(int i = n;i> 0;--i){
         LinkedList p = (LinkedList)malloc(sizeof(LNode));
         p ->data = 0;
-        p -> next = l -> next;l -> next = p;
+        p -> next = l -> next;
+        l -> next = p;
     }
 }
 
@@ -32,5 +40,28 @@ int get(LinkedList l, int i, int *ele){
         return 0;
     }
     (*ele) = p->data;
+    return 1;
+}
+
+int insert(LinkedList l,int i,int e){
+    LinkedList p = l;
+    while(l->next){
+        printf("data:%d",(*l).data);
+        l = l->next;
+    }
+    int j = 0;
+    while(p && j < i-1){
+        p = p ->next;
+        ++j;
+        printf("data:%d",p);
+    }
+    if(!p || j > i-1){
+        printf("if");
+        return -99;
+    }
+    LinkedList s = (LinkedList)malloc(sizeof(LNode));
+    s ->data = e;
+    s ->next = p ->next;
+    p ->next = s;
     return 1;
 }
--- linkedList.h
+++ linkedList.h
@@ -7,10 +7,12 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 typedef struct LNode{
     int data;
     struct LNode * next;
 } LNode, * LinkedList; //* LinkedList是这个结构体的结构指针
 void create(LinkedList l,int n);
 int get(LinkedList l, int i, int *ele);
+int insert(LinkedList l,int i,int e);
 #endif //DATA_STRUCTURE_C_LINKEDLIST_H
