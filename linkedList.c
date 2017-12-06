676b9b4886433efc583aa2307647859e4fb0456c
knockinHeavenDoor <61239289@qq.com>
2017-12-06 22:10:10 +0800

    修复create方法和insert方法

--- linkedList.c
+++ linkedList.c
@@ -5,34 +5,41 @@
 int size;
 int main(){
     LinkedList list;
-    create(list,5);
+    create(&list,5);
+//    while((list->next) != NULL){
+//        printf("data:%d\n",(*list).data);
+//        printf("next:%d\n",(*list).next);
+//        list = list->next;
+//    }
+    printf("---------");
+    insert(&list,3,9);
     while((list->next) != NULL){
-        printf("data:%d",(*list).data);
+        printf("data:%d\n",(*list).data);
+        printf("next:%d\n",(*list).next);
         list = list->next;
     }
-    insert(list,3,9);
-    int h = 3;
-    get(list,3,&h);
+    int h = 1;
+    get(&list,3,&h);
     printf("%d",h);
     return 0;
 }
 
-void create(LinkedList l,int n){
-    l = (LinkedList)malloc(sizeof(LNode));
-    l -> next = NULL;
-    l -> data = 0;
+void create(LinkedList *l,int n){ //????????
+    (*l) = (LinkedList)malloc(sizeof(LNode));
+    (*l) -> next = NULL;
+    (*l) -> data = 0;
     for(int i = n;i> 0;--i){
         LinkedList p = (LinkedList)malloc(sizeof(LNode));
         p ->data = 0;
-        p -> next = l -> next;
-        l -> next = p;
+        p -> next = (*l)->next;
+        (*l) -> next = p;
     }
 }
 
 int get(LinkedList l, int i, int *ele){
     LinkedList p = l ->next;
-    int j = 1;
-    while(p && j < i){
+    int j = 0;
+    while(p && j < i+2){
         p = l ->next;
         ++ j;
     }
@@ -43,20 +50,14 @@ int get(LinkedList l, int i, int *ele){
     return 1;
 }
 
-int insert(LinkedList l,int i,int e){
-    LinkedList p = l;
-    while(l->next){
-        printf("data:%d",(*l).data);
-        l = l->next;
-    }
+int insert(LinkedList *l,int i,int e){
+    LinkedList p = *l;
     int j = 0;
     while(p && j < i-1){
         p = p ->next;
         ++j;
-        printf("data:%d",p);
     }
-    if(!p || j > i-1){
-        printf("if");
+    if(p == NULL || j > i-1){
         return -99;
     }
     LinkedList s = (LinkedList)malloc(sizeof(LNode));
--- linkedList.h
+++ linkedList.h
@@ -12,7 +12,7 @@ typedef struct LNode{
     int data;
     struct LNode * next;
 } LNode, * LinkedList; //* LinkedList是这个结构体的结构指针
-void create(LinkedList l,int n);
+void create(LinkedList *l,int n);
 int get(LinkedList l, int i, int *ele);
-int insert(LinkedList l,int i,int e);
+int insert(LinkedList *l,int i,int e);
 #endif //DATA_STRUCTURE_C_LINKEDLIST_H
