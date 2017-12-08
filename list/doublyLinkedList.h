3ceb84e8c802307df6ba28d6e6fe6590c4513ba2
knockinHeavenDoor <61239289@qq.com>
2017-12-08 23:56:55 +0800

    定义结构体

--- list/doublyLinkedList.h
+++ list/doublyLinkedList.h
@@ -4,5 +4,13 @@
 
 #ifndef DATA_STRUCTURE_C_DOUBLYLINKEDLIST_H
 #define DATA_STRUCTURE_C_DOUBLYLINKEDLIST_H
-
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+typedef struct Node{
+    int data;
+    struct Node * prev;
+    struct Node * next;
+}DuNode, * DuLinkedList;
 #endif //DATA_STRUCTURE_C_DOUBLYLINKEDLIST_H
