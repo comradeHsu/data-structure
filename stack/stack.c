0aa56c16e35696b9e77f3f854423eefec9b17cf3
knockinHeavenDoor <61239289@qq.com>
2017-12-11 21:07:56 +0800

    初始化stack

--- stack/stack.c
+++ stack/stack.c
@@ -2,3 +2,20 @@
 // Created by xuhui on 2017/12/11.
 //
 #include "stack.h"
+
+int main(){
+    struct Stack stack;
+    initStack(&stack);
+    printf("size:%d",stack.size);
+    return 0;
+}
+
+int initStack(Stack * stack){
+    stack->base = (int *)malloc(STACK_INIT_SIZE * sizeof(int));
+    if(!stack->base){
+        return false;
+    }
+    stack->top = stack->base;
+    stack->size = STACK_INIT_SIZE;
+    return true;
+}
--- stack/stack.h
+++ stack/stack.h
@@ -4,11 +4,20 @@
 
 #ifndef DATA_STRUCTURE_C_STACK_H
 #define DATA_STRUCTURE_C_STACK_H
-#define STACK_INIT_SIZE 100;
-#define STACK_PLUS 10;
-typedef struct {
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#define true 1
+#define false 0
+#define STACK_INIT_SIZE 100
+#define STACK_PLUS 10
+
+typedef struct Stack{
     int * base;
     int * top;
     int size;
 } Stack;
+
+int initStack(Stack * stack);
 #endif //DATA_STRUCTURE_C_STACK_H
