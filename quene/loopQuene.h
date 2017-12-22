b42278e1ed7ece0cc667eb6331252599cce58d0b
knockinHeavenDoor <61239289@qq.com>
2017-12-22 23:56:14 +0800

    循环队列定义

---
+++ quene/loopQuene.h
@@ -0,0 +1,19 @@
+//
+// Created by xuhui on 2017/12/22.
+//
+
+#ifndef DATA_STRUCTURE_C_LOOPQUENE_H
+#define DATA_STRUCTURE_C_LOOPQUENE_H
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#define true 1
+#define false 0
+typedef struct {
+    int * base;
+    int front;
+    int rear;
+} loopQuene;
+
+#endif //DATA_STRUCTURE_C_LOOPQUENE_H
