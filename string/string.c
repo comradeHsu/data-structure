8a7f3a1ed590b12fab7480a110c6adbf7a8e0824
knockinHeavenDoor <61239289@qq.com>
2017-12-25 23:30:41 +0800

    串的定义

---
+++ string/string.c
@@ -0,0 +1,8 @@
+//
+// Created by xuhui on 2017/12/25.
+//
+#include "string.h"
+
+int main(){
+    return 0;
+}
---
+++ string/string.h
@@ -0,0 +1,12 @@
+//
+// Created by xuhui on 2017/12/25.
+//
+
+#ifndef DATA_STRUCTURE_C_STRING_H
+#define DATA_STRUCTURE_C_STRING_H
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#endif //DATA_STRUCTURE_C_STRING_H
